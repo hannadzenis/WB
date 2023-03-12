00522579ff7afb6b52653846e1a253185330651b
yury-odinson <yury.odinson@gmail.com>
2023-03-12 15:26:33 +0300

    fix: search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { loadCards } from "./search.js";
+import { renderCards } from "./search.js";
 
 export const cardsURL = 'https://63fb14da2027a45d8d5fb8bf.mockapi.io/cards'
 
-loadCards()
+renderCards()
