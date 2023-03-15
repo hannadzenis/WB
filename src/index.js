5a973551547bf347ddcefd3320b239de857732a9
yury-odinson <yury.odinson@gmail.com>
2023-03-15 22:58:11 +0300

    fix: search + hits

--- src/index.js
+++ src/index.js
@@ -1,5 +1,97 @@
-import { loadCards } from "./search.js";
+import { renderCards } from "./search.js";
 
 export const cardsURL = 'https://63fb14da2027a45d8d5fb8bf.mockapi.io/cards'
 
-loadCards()
+renderCards()
+
+function calcPrice (noDiscount, sale) {
+    let resultPrice = noDiscount - (noDiscount * (sale/100));
+    return resultPrice;
+}
+
+const productGrid = document.querySelector('.products-list')
+
+async function salesHits() {
+    const response = await fetch(cardsURL)
+    const cardsArr = await response.json()
+
+    for (let i = 0; i < 10; i++) {
+        let itemHTML = `
+        <div class="product-list__item">
+    
+            <div class="product-list__item__card">
+    
+                <div class="card__img">
+                    <img src="${cardsArr[i].image}" alt="product">
+                </div>
+    
+                <button class="card__view">Quick view</button>
+    
+                <div class="card__item-sale">-${cardsArr[i].sale}%</div>
+    
+                <button class="card__item-add"></button>
+            </div>
+    
+            <div class="product-list__item__info">
+    
+                <div class="info__price">
+                    <p class="price_number">${calcPrice(cardsArr[i].price, cardsArr[i].sale)} PLN</p>
+                    <p class="price_number_crossed">${cardsArr[i].discountPrice} PLN</p>
+                </div>
+    
+                <div class="info__item-name">
+                    <p class="name_text">${cardsArr[i].name}</p>
+                </div>
+    
+            </div>
+        </div>
+        `
+        productGrid.innerHTML += itemHTML;
+    }
+}
+
+salesHits()
+
+/* POP-UP */
+const buttons = document.querySelectorAll('.card__view');
+const overlay = document.querySelector('.overlay');
+//===========================
+const popUpWindow = document.querySelector('.pop-up__content')
+// console.log(popUpWindow)
+
+
+buttons.forEach( (button) => {
+    button.addEventListener('click', (event) => {
+        overlay.classList.toggle('overlay_active')
+
+        let cardInfo = event.target.closest('.product-list__item').querySelector('.product-list__item__info')
+        let cardPrice = cardInfo.querySelector('.price_number').innerText
+        let cardPriceCrossed = cardInfo.querySelector('.price_number_crossed').innerText
+        let cardTitle = cardInfo.querySelector('.info__item-name').innerText
+        
+        let cardImgSrc = event.target.closest('.product-list__item').querySelector('.card__img img').src
+        // console.log(cardImgSrc)
+
+        let popUpTitle = popUpWindow.querySelector('.pop-up__content-info__title') 
+        let popUpPrice = popUpWindow.querySelector('.info__price_pop-up__price_number')
+        let popUpPriceCrossed = popUpWindow.querySelector('.info__price_pop-up__price_number_crossed')
+        let popUpImg = popUpWindow.querySelector('.pop-up__content-image img')
+        // console.dir(popUpImg)
+
+        // console.log(cardPrice, cardTitle)
+        popUpTitle.innerText = cardTitle;
+        popUpPrice.innerText = cardPrice;
+        popUpPriceCrossed.innerText = cardPriceCrossed;
+        popUpImg.src = cardImgSrc;
+
+    })
+})
+
+const closePopUp = document.querySelector('.pop-up__content__close')
+closePopUp.addEventListener('click', () => {
+    overlay.classList.toggle('overlay_active')
+})
+
+
+
+
--- src/search.js
+++ src/search.js
@@ -3,18 +3,21 @@ import { createCard } from "./cards.js"
 
 const searchElement = document.getElementById('search')
 
-export async function loadCards() {
+export async function renderCards() {
     const response = await fetch(cardsURL)
     const cardsArr = await response.json()
-    function renderCards() {
-        searchElement.onchange = () => {
-            const regex = new RegExp(searchElement.value) // regex = /value from input/
-            for(let i = 0; i < cardsArr.length; i++) {
-                if (regex.test(cardsArr[i].name.toLowerCase())) {
+    searchElement.oninput = () => {
+        const testDel = document.querySelectorAll('.cards-item')
+        testDel.forEach((element) => { element.remove() })
+        const regex = new RegExp(searchElement.value) // regex = /value from input/
+        for(let i = 0; i < cardsArr.length; i++) {
+            if (regex.test(cardsArr[i].name.toLowerCase())) {
+                if (searchElement.value !== '') {
                     createCard(cardsArr[i])
+                } else {
+                    testDel.forEach((element) => { element.remove() })
                 }
             }
         }
     }
-    return renderCards()
 }
