e4e2a5f34a40f09f045687e445f436affb7030c0
Aliaksandr Shcherba <shcherba26@gmail.com>
2023-03-21 21:59:57 +0300

    feat: add adaptive for slider

--- src/slider.js
+++ src/slider.js
@@ -1,11 +1,32 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        origin: "left center",
+        translate: ["-5%", 0, -200],
+        rotate: [0, 100, 0],
+      },
+      next: {
+        origin: "right center",
+        translate: ["5%", 0, -200],
+        rotate: [0, -100, 0],
+      },
+    },
+    spaceBetween: 300,
+      centeredSlides: true,
+      // autoplay: {
+      //   delay: 3500,
+      //   disableOnInteraction: false,
+      // },
     direction: 'horizontal',
     loop: true,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   
     // Navigation arrows
