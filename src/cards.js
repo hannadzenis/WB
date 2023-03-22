d6e136a06ae4caa5f3e82b1f8d47ca2c4c06b531
yury-odinson <yury.odinson@gmail.com>
2023-03-22 12:13:02 +0300

    fix: adaptive styles

--- src/cards.js
+++ src/cards.js
@@ -7,15 +7,19 @@ export function createCard({name, price, image, sale, idElem, description}) {
 
     const cardItem = createElement('div', ['cards-item'], {id: idElem})
         const itemContent = createElement('div', ['item-content'], {})
-            const cardImage = createElement('img', ['item__image'], {})
-            cardImage.src = image
+
+            const cardImageArea = createElement('div', ['item-image-area'], {})
+                const cardImage = createElement('img', ['item__image'], {})
+                cardImage.src = image
+            cardImageArea.append(cardImage)
+
             const itemInfo = createElement('div', ['item-info'], {})
                 const itemName = createElement('div', ['item__name'], {})
                 itemName.innerHTML = name
                 const itemDescription = createElement('div', ['item__descriptions'], {})
                 itemDescription.innerHTML = description
             itemInfo.append(itemName, itemDescription)
-        itemContent.append(cardImage, itemInfo)
+        itemContent.append(cardImageArea, itemInfo)
         
         const itemFooter = createElement('div', ['item-footer'], {})
             const itemAddInBasket = createElement('span', ['item__byu'], {})
